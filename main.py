4acf6ea603f6af0f9e3cab6bf346eb9077fb2242
Martin Angjelevski <angjelevskimartin@gmail.com>
2024-03-08 22:53:19 +0100

    Clarify what is printed

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from functions import *
 if __name__ == '__main__':
   init_dist = init_distribution()
   scaled_dist = scale_distribution(init_dist)
+  print("This is the scaled distribution")
   print(scaled_dist)
   
